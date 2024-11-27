329732bdd2139bde0fcd294a6863aaaa2f772ba4
morfs <morfsevens@outlook.com>
2024-11-27 18:21:06 +0900

    :tada: first commit

---
+++ src/test/java/com/morfs/catalog_service/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morfs.catalog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
