5cacce3f1b6ba6f93bda6c990c360a48275b3f7d
morfs <morfsevens@outlook.com>
2024-11-28 23:27:24 +0900

    :sparkles: Added configue client
    spring cloud 때문에 그래들 버전을 변경해야만 함. 아직 3.4.0 을 지원하지 않음

--- src/main/java/com/morfs/catalog_service/HomeController.java
+++ src/main/java/com/morfs/catalog_service/HomeController.java
@@ -1,14 +1,21 @@
 package com.morfs.catalog_service;
 
+import com.morfs.catalog_service.config.PolarProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    private final PolarProperties polarProperties;
+
+    public HomeController(PolarProperties polarProperties) {
+        this.polarProperties = polarProperties;
+    }
+
 	@GetMapping("/")
 	public String getGreeting() {
-		return "Welcome to the book catalog!";
+        return polarProperties.getGreeting();
 	}
 
 }
